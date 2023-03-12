82d8693701492b8376f5b040c26f0004a4d6ce1c
sarmsoo <rnjsdydwls97@gmail.com>
2023-03-12 22:41:17 +0900

    23/03/12

--- Algo/baek/17626.py
+++ Algo/baek/17626.py
@@ -1,4 +1,19 @@
+# bottom-up
+
 from math import sqrt
 
 n = int(input())
 
+dp = [0] * (n + 1)
+dp[0] = 0
+dp[1] = 1
+
+for i in range(2, n + 1):
+    min_ = 4
+    for j in range(int(sqrt(i)), 0, -1):
+        if j ** 2 > i:
+            break
+        min_ = min(min_, dp[i - j ** 2])
+    dp[i] = min_ + 1
+
+print(dp[n])
