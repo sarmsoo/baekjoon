d26197680c5c982bc257ef28ab91b1327857b777
sarmsoo <rnjsdydwls97@gmail.com>
2023-03-15 22:18:27 +0900

    23/03/15

--- Algo/baek/6064.py
+++ Algo/baek/6064.py
@@ -1,21 +1,18 @@
 from sys import stdin
-import math
 input = stdin.readline
 
 t = int(input())
-
 for _ in range(t):
     m, n, x, y = map(int, input().split())
-    lcm = math.lcm(m, n)
-    flag = False
-    for i in range(lcm // m):
-        answer = m * i + x  
-        for j in range(lcm // n):
-            if answer == n * j + y:
-                print(answer)
-                flag = True
-                break
-        if flag:
+    if x == y:
+        print(x)
+        continue
+
+    flag = True
+    for i in range(n + 1):
+        if (m * i + x - y) % n == 0:
+            print(m * i + x)
+            flag = False
             break
-    if not flag:
+    if flag:
         print(-1)
