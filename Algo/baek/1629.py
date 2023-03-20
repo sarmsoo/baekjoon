d94ff14995573a5c369c3819f3afb56023727bd6
sarmsoo <rnjsdydwls97@gmail.com>
2023-03-20 15:55:25 +0900

    23/03/20

--- Algo/baek/1629.py
+++ Algo/baek/1629.py
@@ -1,2 +1,22 @@
 a, b, c = map(int, input().split())
 
+def sol(n):
+    if n == 1:
+        return a % c
+    else:
+        tmp = sol(n // 2)
+        # 나머지 분배 법칙
+        if n % 2 == 0:
+            return (tmp * tmp) % c
+        else:
+            return (tmp * tmp * a) % c
+
+def sol2(n):
+    if n == 1:
+        return a % c
+
+    tmp = sol(n // 2)
+    return ((tmp % c) * (tmp % c) * ((a ** (n % 2)) % c)) % c # 나머지 분배법칙
+
+print(sol(b))
+print(sol2(b))
