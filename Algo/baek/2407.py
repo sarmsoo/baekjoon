a7c4e885cf74a3545df39638b1820bc3c88a7f45
sarmsoo <rnjsdydwls97@gmail.com>
2023-03-21 14:42:57 +0900

    23/03/21

--- Algo/baek/2407.py
+++ Algo/baek/2407.py
@@ -1,2 +1,12 @@
 n, m = map(int, input().split())
 
+table = [[0] * (i + 1) for i in range(1, n + 1)]
+for i in range(0, n):
+    table[i][0] = 1
+    table[i][-1] = 1
+
+for i in range(0, n):
+    for j in range(1, i + 1):
+        table[i][j] = table[i - 1][j] + table[i - 1][j - 1]
+
+print(table[n - 1][m])
