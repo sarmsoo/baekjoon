28fa4011ed396e29ab27c0a2c2edce04ed68e941
sarmsoo <rnjsdydwls97@gmail.com>
2023-03-28 21:56:04 +0900

    23/03/22

--- Algo/baek/11053.py
+++ Algo/baek/11053.py
@@ -19,13 +19,24 @@ dp = [0] * n
 dp[0] = 1
 
 def sol(i):
+    if i == 0:
+        return 1
     if dp[i] != 0:
         return dp[i]
-
-    for j in range(i):
-        if a[i] > a[j]:
-            dp[i] = max(dp[i], sol(j))
-    dp[i] += 1
+    dp[i] = 1
+    for j in range(i - 1, -1, -1):
+        if dp[i] < sol(j) + 1 and a[i] > a[j]:
+            dp[i] = sol(j) + 1
     return dp[i]
 
-print(sol(n - 1))
+sol(n - 1)
+print(max(dp))
+'''
+top-down 에서
+if a[i] > a[j] and dp[i] < sol(j):
+으로 순서 바꾸면 틀림. 굉장히 주의 해야함.
+이유는 a[i] > a[j]에서 먼저 조건 판단이 나서 
+if 아래의 dp 테이블에 값을 넣지를 못하는 경우가 발생함.
+의도적으로 dp[i] < sol(j)를 조건문에서 먼저 적어주면
+모든 경우의 dp table이 값이 들어감.
+'''
