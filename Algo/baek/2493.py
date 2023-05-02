552be0e18b4dc75c1c5015dbf2589eb559bb340a
sarmsoo <rnjsdydwls97@gmail.com>
2023-05-02 11:44:56 +0900

    23/05/02

---
+++ Algo/baek/2493.py
@@ -0,0 +1,20 @@
+from sys import stdin
+input = stdin.readline
+
+n = int(input())
+top = list(map(int, input().split()))
+stack = []
+answer = []
+
+for i in range(n):
+    while stack:
+        if stack[-1][1] > top[i]:
+            answer.append(stack[-1][0] + 1)
+            break
+        else:
+            stack.pop()
+    if not stack:
+        answer.append(0)
+    stack.append([i, top[i]])
+
+print(*answer)
