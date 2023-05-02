013cbd41350baa338309d61508b341fb78b64491
sarmsoo <rnjsdydwls97@gmail.com>
2023-05-02 18:57:34 +0900

    23/05/02

--- Algo/baek/11048.py
+++ Algo/baek/11048.py
@@ -24,17 +24,13 @@ print(dp[n - 1][m - 1])
 # top-down
 n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
-dp = [[0] * m for _ in range(n)]
-dp[0][0] = graph[0][0]
+dp = [[-1] * m for _ in range(n)]
 
 def sol(y, x):
-    if y == 0 and x == 0:
-        return graph[0][0]
-
-    if y < 0 or y >= n or x < 0 or x >= m:
+    if y < 0 or x < 0:
         return 0
 
-    if dp[y][x] != 0:
+    if dp[y][x] != -1:
         return dp[y][x]
 
     dp[y][x] = graph[y][x] + max(sol(y - 1, x), sol(y, x - 1), sol(y - 1, x - 1))
