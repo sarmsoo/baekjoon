ec0ff447514fde7e2f3fe9c924c8f959bde64814
sarmsoo <rnjsdydwls97@gmail.com>
2023-05-16 17:00:15 +0900

    23/05/16

--- Algo/baek/14002.py
+++ Algo/baek/14002.py
@@ -32,12 +32,11 @@ for i in range(n):
     for j in range(i):
         if arr[i] > arr[j] and dp[i] < dp[j]:
             dp[i] = dp[j]
-        dp[i] += 1:
+        dp[i] += 1
         
 print(max(dp))
 
 k = max(dp)
-
 result = []
 for i in range(n - 1, -1, -1):
     if dp[i] == k:
