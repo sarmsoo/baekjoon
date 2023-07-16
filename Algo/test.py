b21d551f559503631a00f700a4025ecae6770bf9
sarmsoo <rnjsdydwls97@gmail.com>
2023-07-16 21:42:52 +0900

    23/07/16

---
+++ Algo/test.py
@@ -0,0 +1,33 @@
+from sys import stdin
+import math
+input = stdin.readline
+
+for _ in range(int(input())):
+    x1, y1, r1, x2, y2, r2 = map(int, input().split())
+    dist = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+    if dist == 0:
+        if r1 == r2:
+            print(-1)
+            continue
+        if r1 != r2:
+            print(0)
+            continue
+
+    if dist == r1 + r2:
+        print(1)
+        continue
+
+    if dist > r1 + r2:
+        print(0)
+        continue
+
+    if dist < r1 + r2:
+        if dist < abs(r1 - r2):
+            print(0)
+            continue
+        if dist == abs(r1 - r2):
+            print(1)
+            continue
+        if dist > abs(r1 - r2):
+            print(2)
+            continue
