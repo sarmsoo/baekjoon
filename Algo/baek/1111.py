1c7a8c8d3312657767b00c5fb8952544efcb52f8
sarmsoo <rnjsdydwls97@gmail.com>
2023-09-23 23:05:34 +0900

    23/09/23

--- Algo/baek/1111.py
+++ Algo/baek/1111.py
@@ -15,14 +15,17 @@ if n == 2:
         print("A")
     exit(0)
 
+# n >= 2, 처음 두개가 같을 때
 if arr[0] == arr[1]:
     flag = True
     for i in range(2, n):
         if arr[i] != arr[0]:
             flag = False
             break
+    # 모두 같은 숫자일 때
     if flag:
         print(arr[0])
+    # 다른 숫자가 있을 때
     else:
         print("B")
     exit(0)
@@ -36,6 +39,7 @@ for i in range(n - 2):
         a = (arr[i + 2] - arr[i + 1]) // (arr[i + 1] - arr[i])
         b = arr[i + 1] - a * arr[i]
 
+# 구한 a, b가 맞으면 flag = True
 flag = True
 for i in range(n - 1):
     if arr[i + 1] != arr[i] * a + b:
