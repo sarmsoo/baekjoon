4333b7383924fdd79f90abe1afce7207c2535597
sarmsoo <rnjsdydwls97@gmail.com>
2023-10-28 20:41:26 +0900

    23/10/28

--- Algo/baek/21758.py
+++ Algo/baek/21758.py
@@ -10,7 +10,7 @@ for i in range(1, n):
     s[i] += s[i - 1]
 
 for i in range(1, n - 1):
-    answer = max(answer, 2 * s[-1] - arr[-1] - arr[i] - s[i])
+    answer = max(answer, 2 * s[-1] - arr[0] - arr[i] - s[i])
 
 for i in range(1, n - 1):
     answer = max(answer, s[-1] - arr[-1] - arr[i] + s[i - 1])
